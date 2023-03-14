9cdc453140af618ae098dc768c89b56298444bd6
barroscruzc <barroscruzc@gmail.com>
2023-03-14 14:33:49 -0300

    Supplier DAO

---
+++ src/main/java/br/com/univirtus/dao/SupplierDAO.java
@@ -0,0 +1,45 @@
+package br.com.univirtus.dao;
+
+import java.util.List;
+
+import br.com.univirtus.model.Supplier;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+
+public class SupplierDAO implements CRUD<Supplier, Long>{
+
+	@PersistenceContext
+	EntityManager entityManager;
+	
+	@Override
+	public Supplier searchById(Long id) {
+		return entityManager.find(Supplier.class, id);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Supplier> findAll() {
+		Query query = entityManager.createQuery("Select s from Supplier s");
+		return (List<Supplier>) query.getResultList();
+	}
+
+	@Override
+	public void insert(Supplier supplier) {
+		entityManager.persist(supplier);
+	}
+
+	@Override
+	public void update(Supplier supplier) {
+		entityManager.merge(supplier);
+	}
+
+	@Override
+	public void remove(Supplier supplier) {
+		entityManager.remove(supplier);
+	}
+
+
+
+	
+}
