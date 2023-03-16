58a2e0c1d035532958c6b0286a5755ece75c287b
barroscruzc <barroscruzc@gmail.com>
2023-03-16 20:08:18 -0300

    ProductStock DAO

---
+++ src/main/java/br/com/univirtus/dao/ProductStockDAO.java
@@ -0,0 +1,46 @@
+package br.com.univirtus.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.univirtus.model.ProductStock;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+
+@Repository
+public class ProductStockDAO implements CRUD<ProductStock, Long>{
+
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	
+	@Override
+	public ProductStock searchById(Long id) {
+		return entityManager.find(ProductStock.class, id);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<ProductStock> findAll() {
+		Query query = entityManager.createNativeQuery("Select ps from ProductStock ps");
+		return query.getResultList();
+	}
+
+	@Override
+	public void insert(ProductStock productStock) {
+		entityManager.persist(productStock);
+	}
+
+	@Override
+	public void update(ProductStock productStock) {
+		entityManager.merge(productStock);
+	}
+
+	@Override
+	public void remove(ProductStock productStock) {
+		entityManager.remove(productStock);
+	}
+
+}
